9a30d5eab53e8e38b9733267ab3421c6ce1a8de8
Santiago Orona <95342@sistemas.frc.utn.edu.ar>
2025-04-15 19:35:16 -0300

    Subo cambios realizados en los archivos css para el estilo de la user story. Santiago Orona

--- Prueba base React/ecoharmony-web/src/main.jsx
+++ Prueba base React/ecoharmony-web/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Podés usar este archivo para estilos globales si querés
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- Prueba base React/ecoharmony-web/src/services/api.js
+++ Prueba base React/ecoharmony-web/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE = "http://localhost:3000/api"; // ajustá si usás otro puerto
+const API_BASE = "http://localhost:3000/api";
 
 export const comprarEntradas = (datos) => {
   return axios.post(`${API_BASE}/comprar`, datos);
